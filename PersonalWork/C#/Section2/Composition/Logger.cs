ea98c2eb54e863b57be1582949c8b9b2b01d4f8e
jbkim0547 <75588499+jbkim0547@users.noreply.github.com>
2023-01-04 15:04:29 -0600

    Create Logger.cs
    Adding Logger.cs

---
+++ PersonalWork/C#/Section2/Composition/Logger.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Composition
+{
+    public class Logger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
+
+

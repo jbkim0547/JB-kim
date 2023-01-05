6dfe5d259e431b5d4d40107508f76bc802766023
jbkim0547 <75588499+jbkim0547@users.noreply.github.com>
2023-01-05 15:11:02 -0600

    Create Text.cs
    Adding Text.cs

---
+++ PersonalWork/C#/Section2/Inheritance/Text.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Inheritance
+{
+    public class Text: PresentationObject
+    {
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
+
+        public void AddHyperlink(string url)
+        {
+            Console.WriteLine("we added a link to " +url);
+        }
+    }
+}
+
+

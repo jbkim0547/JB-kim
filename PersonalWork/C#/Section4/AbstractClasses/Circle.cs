269abcb272973b68618e92804111f3f0495c9fe5
jbkim0547 <75588499+jbkim0547@users.noreply.github.com>
2023-01-27 11:34:30 -0600

    Create Circle.cs
    Adding Circle.cs

---
+++ PersonalWork/C#/Section4/AbstractClasses/Circle.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AbstractClasses
+{
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a circle");
+        }
+    }
+}

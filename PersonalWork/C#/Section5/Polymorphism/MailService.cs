9ece106f57e1adccc907fefa979569850d640c3c
jbkim0547 <75588499+jbkim0547@users.noreply.github.com>
2023-02-13 11:32:13 -0600

    Create MailService.cs
    Adding MailService.cs

---
+++ PersonalWork/C#/Section5/Polymorphism/MailService.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Polymorphism
+{
+    public class MailService
+    {
+        public void Send(Mail mail)
+        {
+            Console.WriteLine("Sending email...");
+        }
+    }
+}

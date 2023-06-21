4346cc27cb393290590b2ce6587ad7aeecd7e0b9
jbkim0547 <75588499+jbkim0547@users.noreply.github.com>
2023-06-21 13:53:56 -0500

    Create Program.cs
    Adding Program.cs

---
+++ PersonalWork/C#/Section 6/DynamicBinding/Program.cs
@@ -0,0 +1,14 @@
+
+namespace DynamicBinding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+            dynamic d = i;
+            long l = d;
+
+        }
+    }
+}

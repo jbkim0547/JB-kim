2a99458aa96333a17950c878b778f8199cfdeb90
jbkim0547 <75588499+jbkim0547@users.noreply.github.com>
2023-11-08 19:53:29 -0800

    Update HomeController.cs

--- PersonalWork/.Net/BulkyBook/Controllers/HomeController.cs
+++ PersonalWork/.Net/BulkyBook/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 
 namespace BulkyBookWeb.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controllerrr
     {
         private readonly ILogger<HomeController> _logger;
 
